6438ab5b8611beb35a6190f394f05d5459c859ba
lifeart <lifeart92@gmail.com>
2018-11-10 16:30:54 +0300

    examples update

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,6 +1,9 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-
+import { computed } from '@ember/object';
 export default Controller.extend({
-	hotLoader: service()
+	hotLoader: service(),
+	items: computed(function(){
+		return [1,2,3,4,5,6,7,8];
+	})
 });
