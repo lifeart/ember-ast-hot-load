07cf44beacc2cb19dc19296edc68d0167afcecc1
Aleksandr Kanunnikov <lifeart92@gmail.com>
2018-12-23 11:30:08 +0300

    more tests

---
+++ tests/dummy/app/components/unknown-context-wrapper.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import layout from '../templates/components/unknown-context-wrapper';
+
+export default Component.extend({
+  layout,
+  xTestWrapper: 'x-test-wrapper'
+});
--- tests/integration/components/hot-content-test.js
+++ tests/integration/components/hot-content-test.js
@@ -3,6 +3,16 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
+
+function xTestWrapperAssets(el, assert) {
+  assert.equal(el.querySelectorAll('.t-cover').length, 1);
+  assert.equal(el.querySelector('.t-cover').textContent.trim(), 12);
+  assert.equal(el.querySelectorAll('.double-value').length, 2);
+  assert.equal(el.querySelector('.double-value').textContent.trim(), '84');
+  assert.equal(el.querySelectorAll('.value').length, 2);
+  assert.equal(el.querySelector('.value').textContent.trim(), '42');
+}
+
 module('Integration | Component | hot-content', function(hooks) {
   setupRenderingTest(hooks);
 
@@ -19,23 +29,25 @@ module('Integration | Component | hot-content', function(hooks) {
   test('in can hande componenents, passed as @arguments', async function(assert) {
     const el = this.element;
     await render(hbs`{{arg-wrapper itemComponentName="x-test-wrapper"}}`);
-    assert.equal(el.querySelectorAll('.t-cover').length, 1);
-    assert.equal(el.querySelector('.t-cover').textContent.trim(), 12);
-    assert.equal(el.querySelectorAll('.double-value').length, 2);
-    assert.equal(el.querySelector('.double-value').textContent.trim(), '84');
-    assert.equal(el.querySelectorAll('.value').length, 2);
-    assert.equal(el.querySelector('.value').textContent.trim(), '42');
+    xTestWrapperAssets(el, assert);
   });
 
-  test('in can hande componenents, passed as strings to component helper', async function(assert) {
+  test('it can handle components, with name, resolved by unknown context', async function(assert){
+    const el = this.element;
+    await render(hbs`{{unknown-context-wrapper}}`);
+    xTestWrapperAssets(el, assert);
+  });
+
+  test('it can handle components, with name, resolved from context', async function(assert){
+    const el = this.element;
+    await render(hbs`{{context-wrapper}}`);
+    xTestWrapperAssets(el, assert);
+  });
+
+  test('it can hande componenents, passed as strings to component helper', async function(assert) {
     const el = this.element;
     await render(hbs`{{name-wrapper}}`);
-    assert.equal(el.querySelectorAll('.t-cover').length, 1);
-    assert.equal(el.querySelector('.t-cover').textContent.trim(), 12);
-    assert.equal(el.querySelectorAll('.double-value').length, 2);
-    assert.equal(el.querySelector('.double-value').textContent.trim(), '84');
-    assert.equal(el.querySelectorAll('.value').length, 2);
-    assert.equal(el.querySelector('.value').textContent.trim(), '42');
+    xTestWrapperAssets(el, assert);
   });
 
   test('it can handle component attributes', async function(assert) {
@@ -43,12 +55,7 @@ module('Integration | Component | hot-content', function(hooks) {
     // Handle any actions with this.set('myAction', function(val) { ... });
     const el = this.element;
     await render(hbs`{{x-test-wrapper}}`);
-    assert.equal(el.querySelectorAll('.t-cover').length, 1);
-    assert.equal(el.querySelector('.t-cover').textContent.trim(), 12);
-    assert.equal(el.querySelectorAll('.double-value').length, 2);
-    assert.equal(el.querySelector('.double-value').textContent.trim(), '84');
-    assert.equal(el.querySelectorAll('.value').length, 2);
-    assert.equal(el.querySelector('.value').textContent.trim(), '42');
+    xTestWrapperAssets(el, assert);
   });
 
   test('it can handle passed attributes', async function(assert) {
