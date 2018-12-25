810ece449a7925613223497e78c23a02016ab29a
Aleksandr Kanunnikov <lifeart92@gmail.com>
2018-12-25 12:12:15 +0300

    added ember-angle-bracket-invocation-polyfill for tests with older embers

--- config/ember-try.js
+++ config/ember-try.js
@@ -20,6 +20,7 @@ module.exports = function() {
             devDependencies: {
               '@ember/jquery': '^0.5.1',
               'ember-source': '~2.16.0',
+              'ember-angle-bracket-invocation-polyfill': '1.2.3',
               'ember-named-arguments-polyfill': '1.0.0',
               'ember-let-polyfill': '0.1.0'
             }
@@ -34,6 +35,7 @@ module.exports = function() {
             devDependencies: {
               '@ember/jquery': '^0.5.1',
               'ember-source': '~2.18.0',
+              'ember-angle-bracket-invocation-polyfill': '1.2.3',
               'ember-named-arguments-polyfill': '1.0.0',
               'ember-let-polyfill': '0.1.0'
             }
