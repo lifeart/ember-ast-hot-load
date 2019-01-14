63578a258ef727c1ef4453d00bd6ffe58bd7676c
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-01-14 23:07:12 +0300

    registered callbacks counter

---
+++ tests/acceptance/index-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | index', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /index', async function(assert) {
+    await visit('/');
+    const hotLoaderService = this.owner.lookup('service:hot-loader');
+    assert.equal(hotLoaderService._willHotReloadCallbacksCount(), hotLoaderService._willLiveReloadCallbacksCount());
+    assert.equal(hotLoaderService._willLiveReloadCallbacksCount(), 20);
+    assert.equal(currentURL(), '/');
+  });
+});
